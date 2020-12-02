c4591b7b94c8156d7a450f4a8d3dd0101e22e661
Tim881123 <75006110+Tim881123@users.noreply.github.com>
2020-12-02 13:29:00 +0800

    Delete AA.py

--- examples/AA.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-while (1):
-  h0, t0 = Adafruit_DHT.read_retry(sensor, pin)
-  if h0 is not None and t0 is not None:
-      print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0, h0))
-  else:
-      print('Failed to get reading. Try again!')
-      sys.exit(1)
-
